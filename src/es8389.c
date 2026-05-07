4c4829eceb28f5c72d8a560cecd2f3cfd794a876
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2026-05-07 11:00:29 +0800

    Remove license header from es8389.c
    Removed license header and comments from es8389.c

--- src/es8389.c
+++ src/es8389.c
@@ -1,16 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * es8389.c  --  ES8389 ALSA SoC Audio Codec
- *
- * Copyright Everest Semiconductor Co., Ltd
- *
- * Authors:  Michael Zhang (zhangyi@everest-semi.com)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- */
-
 #include <linux/clk.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
